51c554d9cb4f79ccfc16d834132f62a9992f74c0
mursidsaleh55-creator <mursidsaleh55@guru.sma.belajar.id>
2025-10-30 11:49:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama Website PertamaX</title>
+</head>
+<body>
+    <h1>Selamat Datang di Halaman Web PMS</h1>
+    <p>Ini adalah paragraf pertama di halaman utama saya.</p>
+    <p>
+        <a href="tentang.html">Tentang Kami</a> | 
+        <a href="kontak.html">Hubungi Kami</a>
+    </p>
+    <hr>
+    <div>
+        <p>Copyright &copy; 2025 Nama Anda</p>
+    </div>
+</body>
+</html>
